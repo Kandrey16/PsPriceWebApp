ecf3441e7246dfbead8123f6693e33c24e0d5e15
Kandrey16 <isip_a.n.kurdelov@mpt.ru>
2026-01-20 21:18:20 +0300

    components

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,12 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
+import { AppRoot } from '@telegram-apps/telegram-ui'
 
 createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+	<StrictMode>
+		<AppRoot>
+			<App />
+		</AppRoot>
+	</StrictMode>
 )
